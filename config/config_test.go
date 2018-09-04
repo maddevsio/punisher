5539087c29eb2ca797c7e271251437d990782c13
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2018-09-04 17:55:09 +0600

    Fix config tests

--- config/config_test.go
+++ config/config_test.go
@@ -7,12 +7,19 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const BotToken = "testToken"
+const BotChat = "-12345"
+const BotDatabaseURL = "root:root@/interns?parseTime=true"
+
 func TestConfig(t *testing.T) {
-	os.Setenv("TELEGRAM_TOKEN", "value")
+	os.Setenv("BOT_TELEGRAM_TOKEN", BotToken)
+	os.Setenv("BOT_INTERNS_CHAT_ID", BotChat)
+	os.Setenv("BOT_DATABASE_URL", BotDatabaseURL)
 
 	c, err := GetConfig()
 	assert.NoError(t, err)
-	assert.Equal(t, "value", c.TelegramToken)
-	assert.Equal(t, "14:00", c.PunishTime)
+	assert.Equal(t, "testToken", c.TelegramToken)
+	assert.Equal(t, "10:00", c.PunishTime)
+	assert.Equal(t, "pushups", c.PunishmentType)
 
 }
