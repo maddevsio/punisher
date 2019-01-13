89e77c91d91315e95c896d3aba24d38c85653824
Anatoliy Fedorenko <fedorenko.tolik@gmail.com>
2019-01-13 18:24:39 +0600

    Change INT to BIGINT since there might be big chanid number

--- migrations/00003_groupid.sql
+++ migrations/00003_groupid.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- SQL in this section is executed when the migration is applied.
-ALTER TABLE `standup` ADD `groupid` INT NOT NULL;
-ALTER TABLE `interns` ADD `groupid` INT NOT NULL;
+ALTER TABLE `standup` ADD `groupid` BIGINT NOT NULL;
+ALTER TABLE `interns` ADD `groupid` BIGINT NOT NULL;
 -- +goose Down
 -- SQL in this section is executed when the migration is rolled back.
 ALTER TABLE `standup` DROP `groupid`;
